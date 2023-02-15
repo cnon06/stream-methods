9ce200c8c8b1cd7e5b50787207d7b655119f05e1
Sinan Alagöz <sinanalagoz74@gmail.com>
2023-02-15 18:11:44 +0300

    3

--- main.dart
+++ main.dart
@@ -8,22 +8,28 @@ Stream<int> getNumbers() async* {
   }
 }
 
-void main(List<String> args) {
+void main(List<String> args) async {
+  var myStream = getNumbers().asBroadcastStream();
+
+  myStream.listen((event) {
+    print('Stream1: $event');
+  });
+
+  myStream.listen((event) {
+    print('Stream2: $event');
+  });
+//  print("first: ${await myStream.first}");
+  // print("isBroadcast: ${await myStream.isBroadcast}");
+  
+  // print("last: ${await myStream.last}");
+
+  // print("length: ${await myStream.length}");
+
+// print("isContains 3: ${await myStream.contains(3)}");
+// print("isContains 6: ${await myStream.contains(6)}");
+// print("any: ${await myStream.any((element) => element==2)}");
+// print("any: ${await myStream.any((element) => element==99)}");
+print("join: ${await myStream.join(',')}");
+
  
-
-  var subscription = getNumbers().listen((event) {
-    print(event);
-  });
-
-  subscription.onData((data) {
-    print('second data: $data');
-  });
-
-  subscription.onError((err) {
-    print(err);
-  });
-
-  subscription.onDone(() {
-    print('Stream is done.');
-  });
 }
